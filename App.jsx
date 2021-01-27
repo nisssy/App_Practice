63ce7f98d97073901198f71f3d7509e9105cb3d6
kazuhiro.nishikawa <kazuhiro.nishikawa.96@gmail.com>
2021-01-27 17:52:45 +0900

    style memolist

--- App.jsx
+++ App.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Hello from './src/components/Hello';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello style={{ color: 'red', fontSize: 40 }}>World</Hello>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItems}>
+          <View>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <View style={styles.memoListItems}>
+          <View>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <View style={styles.memoListItems}>
+          <View>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>
+          +
+        </Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +57,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+  memoListItems: {
     backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
     alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoListTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467fd3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
